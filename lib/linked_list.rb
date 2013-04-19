e844641c611d714ff491c640a5fbc09503e2e0a0
dmoore5050 <dustinmoore@gmail.com>
2013-04-19 12:50:25 -0500

    All tests passing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -85,19 +85,66 @@ class LinkedList
     current_item.payload
   end
 
-  def []= n, new_payload
-    current_item = @first_item
-    n.times do
-      if n != 0 && current_item != nil
-        current_item = current_item.next_list_item 
+  def []= n, payload
+    new_item, current_item, item_after_new  = LinkedListItem.new( payload ), @first_item, @first_item
+
+    (n + 1).times do
+      if n != 0 && item_after_new != nil
+        item_after_new = item_after_new.next_list_item 
       else
         raise IndexError
       end
     end
-    current_item.payload = new_payload
-  end
 
-  def remove(n)
-  end
+    if n != 0
+      (n - 1).times do
+        if n != 0 && current_item != nil
+          current_item = current_item.next_list_item 
+        else
+          raise IndexError
+        end
+      end
+      current_item.next_list_item = new_item
+    else
+      @first_item = new_item
+    end
+    new_item.next_list_item = item_after_new
 
+  end
+#pseudo - go to item before item being removed, and redirect next_item to item after item being removed
+  def remove n
+    new_next_item, current_item  =  @first_item, @first_item
+
+    (n + 1).times do
+      if new_next_item != nil
+        new_next_item = new_next_item.next_list_item 
+      else
+        raise IndexError
+      end
+    end
+
+    if n > 1
+      (n - 2).times do
+        if current_item != nil
+          current_item = current_item.next_list_item 
+        else
+          raise IndexError
+        end
+      end
+      current_item = current_item.next_list_item
+      current_item.next_list_item = new_next_item
+    elsif n == 0
+      @first_item = new_next_item
+    else
+      
+      @first_item.next_list_item = new_next_item
+    end
+  end
 end
+
+
+
+
+
+
+
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,11 +2,11 @@ class LinkedListItem
   attr_reader :next_list_item
   attr_reader :payload
 
-  def initialize( payload )
+  def initialize payload 
   	@payload = payload
   end
 
-  def next_list_item= ( linked_list_item )
+  def next_list_item= linked_list_item 
   	if self != linked_list_item
   		@next_list_item = linked_list_item
   	else
