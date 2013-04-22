bc529b8f5a152a3eb48f3a27bc015dad1c1b04a5
dmoore5050 <dustinmoore@gmail.com>
2013-04-22 11:36:52 -0500

    refactored to reduce length of code

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -27,109 +27,79 @@ class LinkedList
   def get n 
     current_item = @first_item
     n.times do
-      if n != 0 && current_item != nil
-        current_item = current_item.next_list_item 
-      else
-        raise IndexError
-      end
+      raise IndexError if n == 0 || current_item == nil 
+      current_item = current_item.next_list_item 
     end
     current_item.payload
   end
 
   def last
-      if @first_item != nil
-        final_item = @first_item
-        while !final_item.last?
-          final_item = final_item.next_list_item
-        end
-        final_item.payload
-      end  
+    if @first_item != nil
+      final_item = @first_item
+      while !final_item.last?
+        final_item = final_item.next_list_item
+      end
+      final_item.payload
+    end  
   end
 
   def size
-    list_size, current_item = 0, @first_item
+    list_length, current_item = 0, @first_item
     while current_item != nil
       current_item = current_item.next_list_item 
-      list_size += 1
+      list_length += 1
     end
-    list_size
+    list_length
   end
 
-  def to_s
-    stringified_list, current_item = String.new, @first_item
-    if current_item != nil
-      stringified_list << "| "
-      while current_item != nil
-        stringified_list << current_item.payload
-        stringified_list << ", " if !current_item.last?  
-        current_item = current_item.next_list_item 
-      end
-      stringified_list << " |"
-    else
-      stringified_list << "| |"
-    end  
-    stringified_list
+    def to_s
+    list_items, current_item = String.new, @first_item
+    while current_item != nil
+      list_items << " "
+      list_items << current_item.payload
+      list_items << "," unless current_item.last?  
+      current_item = current_item.next_list_item 
+    end 
+    "|#{list_items} |"
   end
 
   # ========= Bonus ========== #
 
   def [] n
-    current_item = @first_item
-    n.times do
-      if n != 0 && current_item != nil
-        current_item = current_item.next_list_item 
-      else
-        raise IndexError
-      end
-    end
-    current_item.payload
+    self.get n
   end
 
   def []= n, payload
     new_item, current_item, item_after_new  = LinkedListItem.new( payload ), @first_item, @first_item
-
     (n + 1).times do
-      if n != 0 && item_after_new != nil
-        item_after_new = item_after_new.next_list_item 
-      else
-        raise IndexError
-      end
+      raise IndexError if n == 0 || item_after_new == nil 
+      item_after_new = item_after_new.next_list_item 
     end
 
     if n != 0
       (n - 1).times do
-        if n != 0 && current_item != nil
-          current_item = current_item.next_list_item 
-        else
-          raise IndexError
-        end
+        raise IndexError if n == 0 || current_item == nil 
+        current_item = current_item.next_list_item 
       end
       current_item.next_list_item = new_item
     else
       @first_item = new_item
     end
     new_item.next_list_item = item_after_new
-
   end
-#pseudo - go to item before item being removed, and redirect next_item to item after item being removed
+
   def remove n
     new_next_item, current_item  =  @first_item, @first_item
 
     (n + 1).times do
-      if new_next_item != nil
-        new_next_item = new_next_item.next_list_item 
-      else
-        raise IndexError
-      end
+      raise IndexError if new_next_item == nil
+      new_next_item = new_next_item.next_list_item 
     end
 
     if n > 0
       (n - 1).times do
-        if current_item != nil
-          current_item = current_item.next_list_item 
-        else
-          raise IndexError
-        end
+        raise IndexError if current_item == nil
+        current_item = current_item.next_list_item 
       end
       current_item.next_list_item = new_next_item
     else
